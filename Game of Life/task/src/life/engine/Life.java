0e26d968e4c39e16738913fe60a39d567e10a2f6
nkarl <nordenk@pm.me>
2021-07-05 11:48:33 -0700

    updated

--- Game of Life/task/src/life/engine/Life.java
+++ Game of Life/task/src/life/engine/Life.java
@@ -43,10 +43,9 @@ public class Life extends Model implements View {
      */
     void generate() {
         for (int i = 0; i < this.size - 1; ++i) {
-            for (int j = 0; j < this.size - 1; ++j) {
+            for (int j = 0; j < this.size - 1; ++j)
                 this.map[i][j] = (liveOrDie(i, j) == STATE.ALIVE) ?
                         1 : 0;
-            }
         }
     }
 
